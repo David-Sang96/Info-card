ee372a818d5eccae364c34c4c4e3b28885421a2b
David-Sang96 <luainawl@gmail.com>
2024-01-03 02:29:22 +0800

    added useRef

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,26 +1,39 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const Create = () => {
-  const defaultData = {
-    id: Math.floor(Math.random() * 100),
-    name: "",
-    address: "",
-  };
-  const [info, setInfo] = useState(defaultData);
+  // const defaultData = {
+  //   id: Math.floor(Math.random() * 100),
+  //   name: "",
+  //   address: "",
+  // };
+  // const [info, setInfo] = useState(defaultData);
   const [data, setData] = useState([]);
-  console.log(data);
+  const nameInput = useRef();
+  const addressInput = useRef();
 
   const handleCreate = (e) => {
     e.preventDefault();
-    if (info.name === "") {
-      setInfo(defaultData);
-      return;
-    } else if (info.address === "") {
-      setInfo(defaultData);
-      return;
+    // if (info.name === "") {
+    //   setInfo(defaultData);
+    //   return;
+    // } else if (info.address === "") {
+    //   setInfo(defaultData);
+    //   return;
+    // }
+    // setData((prev) => [...prev, info]);
+    // setInfo(defaultData);
+    const data = {
+      name: nameInput.current.value,
+      address: addressInput.current.value,
+    };
+    if (data.name.length === 0 || data.address.length === 0) {
+      nameInput.current.value = "";
+      addressInput.current.value = "";
+      return alert("Pls fill out all fields");
     }
-    setData((prev) => [...prev, info]);
-    setInfo(defaultData);
+    setData((prev) => [...prev, data]);
+    nameInput.current.value = "";
+    addressInput.current.value = "";
   };
 
   return (
@@ -28,15 +41,19 @@ const Create = () => {
       <div className="input">
         <input
           type="text"
-          value={info.name}
-          onChange={(e) => setInfo({ ...info, name: e.target.value })}
+          // value={info.name}
+          // onChange={(e) => setInfo({ ...info, name: e.target.value })}
           placeholder="name..."
+          value={data.name}
+          ref={nameInput}
         />
         <input
           type="text"
-          value={info.address}
-          onChange={(e) => setInfo({ ...info, address: e.target.value })}
+          // value={info.address}
+          // onChange={(e) => setInfo({ ...info, address: e.target.value })}
           placeholder="address..."
+          value={data.address}
+          ref={addressInput}
         />
         <div>
           <button type="submit">create</button>
@@ -45,7 +62,7 @@ const Create = () => {
       <div className="info-container">
         {data.length ? (
           data.map((item) => (
-            <div key={item.id} className=" card">
+            <div key={item.name} className=" card">
               <div>{item.name}</div>
               <div>{item.address}</div>
             </div>
